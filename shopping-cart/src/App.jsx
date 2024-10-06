a8bfb02200bea7c2ca30790e1567c00506572f5e
xEmre1-53 <157003672+xEmre1-53@users.noreply.github.com>
2024-10-06 05:21:53 +0300

    first commit

---
+++ shopping-cart/src/App.jsx
@@ -0,0 +1,15 @@
+import axios from "axios";
+import { Container, Row } from "react-bootstrap";
+import Navbar from "../src/components/Navbar";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+function App() {
+  return (
+    <>
+      <header>
+        <Navbar />
+      </header>
+    </>
+  );
+}
+
+export default App;
