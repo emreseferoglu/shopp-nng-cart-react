001d967bb1f717e077a8b89fc234544c85424d47
xEmre1-53 <157003672+xEmre1-53@users.noreply.github.com>
2024-10-06 08:09:27 +0300

    second

--- shopping-cart/src/components/Product.jsx
+++ shopping-cart/src/components/Product.jsx
@@ -18,26 +18,34 @@ function Product({ searchTerm }) {
   const [error, setError] = useState("");
 
   useEffect(() => {
-    try {
-      const getAxiosProduct = async () => {
+    const getAxiosProduct = async () => {
+      setLoading(true);
+      setError("");
+
+      try {
         const response = await axios.get(
           `https://api.mercadolibre.com/sites/MLA/search?q=${searchTerm}`
         );
         const data = await response.data.results;
-        console.log(data);
-        setProducts(data);
-        setLoading(true);
-        setError("");
-      };
+        if (data.length > 0) {
+          setProducts(data);
+        } else {
+          setProducts([]);
+          setError(`${searchTerm} İlgili Ürün bulunamadı `);
+        }
+      } catch (err) {
+        console.log("API HATASI" + err);
+        setError("Bir hata oluştu tekrar deneyin");
+      } finally {
+        setLoading(false);
+      }
+    };
+    if (searchTerm) {
       getAxiosProduct();
-    } catch (error) {
-      console.error("API HATASI YAKALANDI" + error);
-      setError(error);
-      setLoading(false);
     }
   }, [searchTerm]);
 
-  if (!loading)
+  if (loading)
     return (
       <div className="d-flex justify-content-center">
         <div style={{ position: "absolute", top: "30%", textAlign: "center" }}>
@@ -71,24 +79,37 @@ function Product({ searchTerm }) {
 
   return (
     <div>
-      <Container>
+      <Container className="my-5">
         <Row>
-          {products.map((product) => (
-            <Col sm={12} lg={4} md={4} xl={4} key={product.id}>
-              <Card
-                style={{
-                  height: "350px",
-                  width: "250px",
-                  boxShadow: "0 4px 9px black",
-                  backgroundColor: "transparent",
-                  marginBottom: "25px",
-                }}
-              >
-                <CardTitle>
-                  <CardImg top className="product-image" src={product.thumbnail}></CardImg>
+          {products?.map((product) => (
+            <Col sm={12} lg={4} md={6} xl={4} key={product.id}>
+              <Card className="Card">
+                <CardTitle style={{ padding: "0" }}>
+                  <CardImg
+                    top
+                    className="product-image"
+                    src={product.thumbnail}
+                  ></CardImg>
                 </CardTitle>
                 <CardBody>
                   <CardText>{product.title}</CardText>
+                  <CardText>
+                    Fiyat: <strong>{product.price}</strong>
+                  </CardText>
+
+                  <CardText>
+                    Taksit Oranı: <strong>{product.installments.rate}</strong>
+                  </CardText>
+                  <CardText>
+                    Her Taksitte Ödenecek tutar:{" "}
+                    <strong>{product.installments.amount}</strong>
+                  </CardText>
+                  <hr></hr>
+                  <h5 className="text-center mb-3">Satıcı Bilgileri:</h5>
+                  <CardText>
+                    Adı: <strong>{product.seller.nickname} </strong>
+                  </CardText>
+                  <CardText>{`Puan: ${product.rating}`}</CardText>
                 </CardBody>
               </Card>
             </Col>
